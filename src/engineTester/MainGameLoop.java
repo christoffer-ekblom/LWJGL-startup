470d302131ee2bc047cc9e1721ecbb1b40828b62
sticky4loop <christoffer-ekblom@ChristoffersMBP.lan>
2016-10-12 20:50:30 +0200

    Texturing

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -3,11 +3,13 @@ package engineTester;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.GL11;
 
+import models.RawModel;
+import models.TexturedModel;
 import renderEngine.DisplayManager;
 import renderEngine.Loader;
-import renderEngine.RawModel;
 import renderEngine.Renderer;
 import shaders.StaticShader;
+import textures.ModelTexture;
 
 public class MainGameLoop {
 
@@ -19,10 +21,10 @@ public class MainGameLoop {
 		StaticShader shader = new StaticShader();
 		
 		float[] vertices = {			
-				-0.5f,0.5f,0,	//V0
-				-0.5f,-0.5f,0,	//V1
-				0.5f,-0.5f,0,	//V2
-				0.5f,0.5f,0		//V3
+				1.0f,-1.0f,0,	//V0
+				1.0f,1.0f,0,	//V1
+				-1.0f,1.0f,0,	//V2
+				-1.0f,-1.0f,0		//V3
 		};
 		
 		int[] indices = {
@@ -30,13 +32,22 @@ public class MainGameLoop {
 				3,1,2	//Bottom right triangle (V3,V1,V2)
 		};
 		
-		RawModel model = loader.loadToVAO(vertices,indices);
+		float[] textureCoords = {
+				0,0,
+				0,1,
+				1,1,
+				1,0
+		};
+		
+		RawModel model = loader.loadToVAO(vertices,textureCoords,indices);
+		ModelTexture texture = new ModelTexture(loader.loadTexture("image"));
+		TexturedModel texturedModel = new TexturedModel(model, texture);
 		
 		while(!Display.isCloseRequested()){
 			//game logic
 			renderer.prepare();
 			shader.start();
-			renderer.render(model);
+			renderer.render(texturedModel);
 			shader.stop();
 			DisplayManager.updateDisplay();			
 		}
